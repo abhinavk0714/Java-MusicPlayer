cdeb13380dff840435260e84c491bd1dd3eba34d
Abhinav Krishniah <abhinavk@email.sc.edu>
2024-07-23 18:43:53 -0400

    finished basic GUI

--- src/MusicPlayerGUI.java
+++ src/MusicPlayerGUI.java
@@ -1,4 +1,5 @@
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.image.*;
 import java.io.File;
 
@@ -46,6 +47,32 @@ public class MusicPlayerGUI extends JFrame {
         JLabel songImage = new JLabel(loadImage("src/assets/record.png"));
         songImage.setBounds(0, 50, getWidth() - 20, 225);
         add(songImage);
+        
+        // song title
+        JLabel songTitle = new JLabel("Song Title");
+        songTitle.setBounds(0, 285, getWidth() - 10, 30);
+        songTitle.setFont(new Font("Dialog", Font.BOLD, 24));
+        songTitle.setForeground(TEXT_COLOR);
+        songTitle.setHorizontalAlignment(SwingConstants.CENTER);
+        add(songTitle);
+        
+        // song artist
+        JLabel songArtist = new JLabel("Artist");
+        songArtist.setBounds(0, 315, getWidth() - 10, 30);
+        songArtist.setFont(new Font("Dialog", Font.PLAIN, 24));
+        songArtist.setForeground(TEXT_COLOR);
+        songArtist.setHorizontalAlignment(SwingConstants.CENTER);
+        add(songArtist);
+        
+        // playback slider
+        JSlider playbackSlider = new JSlider(JSlider.HORIZONTAL, 0, 100, 0);
+        playbackSlider.setBounds(getWidth()/2 - 300/2, 365, 300, 40);
+        playbackSlider.setBackground(null);
+        add(playbackSlider);
+        
+        
+        // playback buttons (prev, play, pause, etc.)
+        addPlaybackBtns();
     }
     
     private void addToolbar() {
@@ -81,6 +108,39 @@ public class MusicPlayerGUI extends JFrame {
         add(toolBar);
     }
     
+    private void addPlaybackBtns() {
+        JPanel playbackBtns = new JPanel();
+        playbackBtns.setBounds(0, 435, getWidth() - 10, 80);
+        playbackBtns.setBackground(null);
+        
+        // previous button
+        JButton prevButton = new JButton(loadImage("src/assets/previous.png"));
+        prevButton.setBorderPainted(false);
+        prevButton.setBackground(null);
+        playbackBtns.add(prevButton);
+        
+        // play button
+        JButton playButton = new JButton(loadImage("src/assets/play.png"));
+        playButton.setBorderPainted(false);
+        playButton.setBackground(null);
+        playbackBtns.add(playButton);
+        
+        // pause button
+        JButton pauseButton = new JButton(loadImage("src/assets/pause.png"));
+        pauseButton.setBorderPainted(false);
+        pauseButton.setBackground(null);
+        pauseButton.setVisible(false);
+        playbackBtns.add(pauseButton);
+        
+        // next button
+        JButton nextButton = new JButton(loadImage("src/assets/next.png"));
+        nextButton.setBorderPainted(false);
+        nextButton.setBackground(null);
+        playbackBtns.add(nextButton);
+
+        add(playbackBtns);
+    }
+    
     private ImageIcon loadImage(String imagePath) {
         try{
             // read the image file from the given path
@@ -92,7 +152,7 @@ public class MusicPlayerGUI extends JFrame {
         }catch(Exception e){
             e.printStackTrace();
         }
-
+        
         // could not find resource
         return null;
     } 
